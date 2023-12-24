f3a0ec5be5c3d6d760368c8c54818a834bf31ac1
Moisés Jorge <moisesjorge30@gmail.com>
2023-12-24 18:17:13 +0100

    Criando e executando Factory
    Criei o factory: onde se constroi a estrutura de como os dados vão ser inseridos (preenchimento das colunas/atributos)
    
    Criei a Seeder que vai executar/inserir os dados dessa factory
    
    Modifiquei a definição do database para executar somente a última seed criada. De modos a executar uma seed q já foi executada antes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Categoria;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         
         $this->call([
-            UsersSeeder::class,
+            //UsersSeeder::class, //Comentou-se essa linha para essa seeder nao ser executada duas vezes
+            CategoriasSeeder::class,
         ]);
     }
 }
