bdf52606f63aab4c25e2cee7a7102bcb88b30b6d
Moisés Jorge <moisesjorge30@gmail.com>
2023-12-27 12:26:54 +0100

    Implentando Framework CSS (Materializa)
    Implentação via CDN
    
    Adição e modificação do NavBar (barra de navegação)
    
    Adição, modificação e configuração dos produtos
    
    Instalamos os icons do materialize para podermos usar na nossa aplicao (via CDN tambem)

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,5 +3,61 @@
 @section('title', 'Essa eh a pagina HOME')
 
 @section('conteudo')
-    <h1>Essa eh nossa home</h1>
+    <div class="row container"> {{-- Definição da Linha. O Materialize já traz uma estrutura responsiva --}}
+        {{-- Coluna 1: Dispositivos com tela pequena a largura será de "12" e nos de tela média será "3" --}}
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">visibility</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+
+        {{-- Coluna 2: Dispositivos com tela pequena a largura será de "12" e nos de tela média será "3" --}}
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+
+        {{-- Coluna 3: Dispositivos com tela pequena a largura será de "12" e nos de tela média será "3" --}}
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+
+        {{-- Coluna 4: Dispositivos com tela pequena a largura será de "12" e nos de tela média será "3" --}}
+        <div class="col s12 m3">
+            <div class="card">
+                <div class="card-image">
+                  <img src="images/sample-1.jpg">
+                  <span class="card-title">Card Title</span>
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -5,8 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> @yield('title') </title>
+    <!-- Compiled and minified CSS. Framework CSS Materialize; Instalação via CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <!-- ICONS DO MATERIALIZE -->
 </head>
 <body>
+
+    <nav class="red">
+        <div class="nav-wrapper container">
+          <a href="#" class="brand-logo center">CursoLaravel</a>
+          <ul id="nav-mobile" class="left">
+            <li><a href="">Home</a></li>
+            <li><a href="">Carrinho</a></li>
+          </ul>
+        </div>
+      </nav>
+
     @yield('conteudo')
+
+    <!-- Compiled and minified JavaScript. Framework CSS Materialize; Instalação via CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
