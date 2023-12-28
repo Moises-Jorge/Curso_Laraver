7a20d02f5172fa9b303c79b6fda086be77ec4e20
Moisés Jorge <moisesjorge30@gmail.com>
2023-12-28 16:26:20 +0100

    Implementando Dropdown: Categorias Dinamicas

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -11,11 +11,19 @@
 </head>
 <body>
 
+    <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+      @foreach ($categoriasMenu as $categoriaM)
+        <li><a href=""> {{ $categoriaM->nome }} </a></li>
+      @endforeach
+    </ul>
+
     <nav class="red">
         <div class="nav-wrapper container">
           <a href="#" class="brand-logo center">CursoLaravel</a>
           <ul id="nav-mobile" class="left">
             <li><a href="">Home</a></li>
+            <li><a href="" class="dropdown-trigger" data-target='dropdown1'> Categoria <i class="material-icons right">expand_more</i> </a></li> <!-- Dropdown Trigger Implementation -->
             <li><a href="">Carrinho</a></li>
           </ul>
         </div>
@@ -25,5 +33,14 @@
 
     <!-- Compiled and minified JavaScript. Framework CSS Materialize; Instalação via CDN -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script>
+      /* Dropdown Initialization */
+      var elemDrop = document.querySelectorAll('.dropdown-trigger');
+      var instanceDrop = M.Dropdown.init(elemDrop, {
+          coverTrigger: false,
+          constrainWidth: false
+      });
+    </script>
 </body>
 </html>
